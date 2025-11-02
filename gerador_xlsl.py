1fb66d6e162d3431298c9bae2f0bcec08c93d697
alyff <alyffantonio@gmail.com>
2025-11-02 19:29:13 -0300

    gerador de planilha

--- gerador_xlsl.py
+++ gerador_xlsl.py
@@ -1,31 +1,23 @@
 import pandas as pd
 
 filmes = [
-    "Star Wars: Episode I – The Phantom Menace ",
-    "Star Wars: Episode II – Attack of the Clones ",
-    "Star Wars: Episode III – Revenge of the Sith ",
-    "Star Wars: Episode IV – A New Hope ",
-    "Star Wars: Episode V – The Empire Strikes Back ",
-    "Star Wars: Episode VI – Return of the Jedi ",
-    "Star Wars: Episode VII – The Force Awakens ",
-    "Star Wars: Episode VIII – The Last Jedi ",
-    "Star Wars: Episode IX – The Rise of Skywalker ",
-    "Rogue One: A Star Wars Story ",
-    "Solo: A Star Wars Story "
+"Star Wars: Episode I – The Phantom Menace",
+"Star Wars: Episode II – Attack of the Clones",
+"Star Wars: Episode III – Revenge of the Sith",
+"Star Wars: Episode IV – A New Hope" ,
+"Star Wars: Episode V – The Empire Strikes Back",
+"Star Wars: Episode VI – Return of the Jedi",
+"Star Wars: Episode VII – The Force Awakens",
+"Star Wars: Episode VIII – The Last Jedi",
+"Star Wars: Episode IX – The Rise of Skywalker",
+"Rogue One: A Star Wars Story",
+"Solo: A Star Wars Story",
 ]
-# filmes = [
-#     "A Era do Gelo",
-#     "A Era do Gelo 2: O Degelo",
-#     "A Era do Gelo 3: Despertar dos Dinossauros",
-#     "A Era do Gelo 4: Deriva Continental",
-#     "A Era do Gelo: O Big Bang",
-#     "A Era do Gelo: As Aventuras de Buck"
-# ]
-df = pd.DataFrame(filmes, columns=['Titulo'])
+
+df = pd.DataFrame(filmes)
 
 nome_arquivo = "filmes_star_wars.xlsx"
-# nome_arquivo = "era_do_gelo.xlsx"
 
-df.to_excel(nome_arquivo, index=False)
+df.to_excel(nome_arquivo, index=False, header=False)
 
 print(f"Arquivo '{nome_arquivo}' gerado com sucesso!")
