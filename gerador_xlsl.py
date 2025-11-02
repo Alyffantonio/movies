d03c85aaa19ac15a3c81bb817a3cb26c6f45c176
alyff <alyffantonio@gmail.com>
2025-11-02 17:04:36 -0300

    ajustes requiriments

--- gerador_xlsl.py
+++ gerador_xlsl.py
@@ -1,16 +1,22 @@
 import pandas as pd
 
 filmes = [
-    "Blade Runner",
-    "The Matrix",
-    "Inception",
+    "Star Wars: Episode I – The Phantom Menace ",
+    "Star Wars: Episode II – Attack of the Clones ",
+    "Star Wars: Episode III – Revenge of the Sith ",
+    "Star Wars: Episode IV – A New Hope ",
+    "Star Wars: Episode V – The Empire Strikes Back ",
+    "Star Wars: Episode VI – Return of the Jedi ",
+    "Star Wars: Episode VII – The Force Awakens ",
+    "Star Wars: Episode VIII – The Last Jedi ",
+    "Star Wars: Episode IX – The Rise of Skywalker ",
+    "Rogue One: A Star Wars Story ",
+    "Solo: A Star Wars Story "
 ]
-
 
 df = pd.DataFrame(filmes, columns=['Titulo'])
 
 nome_arquivo = "filmes.xlsx"
-
 
 df.to_excel(nome_arquivo, index=False)
 
